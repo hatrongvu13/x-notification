77a1033dee07dac5e2aaf0e8063514a5fac3bc5c
hatrongvu13 <vuht7@msb.com.vn>
2025-11-07 15:40:34 +0700

    config git action

--- src/main/java/com/htv/notification/config/security/GrpcSecurityConfig.java
+++ src/main/java/com/htv/notification/config/security/GrpcSecurityConfig.java
@@ -1,4 +1,44 @@
 package com.htv.notification.config.security;
 
+import com.htv.notification.config.grpc.GrpcClientJwtInterceptor;
+import com.htv.notification.config.grpc.GrpcServerJwtInterceptor;
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.client.interceptor.GlobalClientInterceptorConfigurer;
+import net.devh.boot.grpc.server.interceptor.GlobalServerInterceptorConfigurer;
+import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+import javax.annotation.Nullable;
+
+@Configuration
+@RequiredArgsConstructor
 public class GrpcSecurityConfig {
+    private final GrpcClientJwtInterceptor grpcClientJwtInterceptor;
+    private final GrpcServerJwtInterceptor grpcServerJwtInterceptor;
+
+    @Bean
+    public GrpcAuthenticationReader grpcAuthenticationReader() {
+        return new GrpcAuthenticationReader() {
+            @Nullable
+            @Override
+            public Authentication readAuthentication(ServerCall<?, ?> call, Metadata headers) throws AuthenticationException {
+                return null;
+            }
+        };
+    }
+
+    @Bean
+    public GlobalClientInterceptorConfigurer globalClientInterceptorConfigurer() {
+        return registry -> registry.add(grpcClientJwtInterceptor);
+    }
+
+    @Bean
+    public GlobalServerInterceptorConfigurer globalServerInterceptorConfigurer() {
+        return registry -> registry.add(grpcServerJwtInterceptor);
+    }
 }
--- src/main/java/com/htv/notification/config/security/WebSecurityConfig.java
+++ src/main/java/com/htv/notification/config/security/WebSecurityConfig.java
@@ -1,4 +1,76 @@
 package com.htv.notification.config.security;
 
+import com.htv.notification.config.jwt.JwtAuthentication;
+import com.htv.notification.config.jwt.JwtFilter;
+import com.htv.notification.config.jwt.TokenProvider;
+import com.htv.notification.config.properties.SecurityProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthentication authentication;
+    private final SecurityProperties securityProperties;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> corsConfigurationSource())
+//                .cors(Customizer.withDefaults())
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(authentication))
+                .authorizeHttpRequests(authentication ->
+                        {
+                            if (!CollectionUtils.isEmpty(securityProperties.getPermitAll())) {
+                                securityProperties.getPermitAll().forEach(securityPermit -> authentication.requestMatchers(securityPermit).permitAll());
+                            }
+                            authentication.anyRequest().authenticated();
+                        }
+
+                )
+                .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("demo").password("demo").roles("USER").build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        if (securityProperties.getCrossOrigin().size() == 1) {
+            configuration.setAllowedOrigins(securityProperties.getCrossOrigin());
+        } else {
+            configuration.addAllowedOrigin(securityProperties.getCrossOrigin().get(0));
+        }
+        configuration.setAllowedMethods(securityProperties.getAllowedMethods());
+        configuration.setAllowedHeaders(securityProperties.getAllowedHeaders());
+        configuration.setAllowCredentials(securityProperties.isCredentials());
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
