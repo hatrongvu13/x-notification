04baba71fdd9b8ce03957733e392bb86ff4c33f6
hatrongvu13 <hatrongvu13@gmail.com>
2025-11-07 19:56:33 +0700

    config telegram

--- src/main/java/com/htv/notification/config/telegram/JaxtonyBot.java
+++ src/main/java/com/htv/notification/config/telegram/JaxtonyBot.java
@@ -1,4 +1,61 @@
 package com.htv.notification.config.telegram;
 
-public class JaxtonyBot {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+@Component
+public class JaxtonyBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
+    private final TelegramClient telegramClient;
+
+    public JaxtonyBot( @Value("telegram.bot.token") String token) {
+        this.telegramClient = new OkHttpTelegramClient(getBotToken());
+        this.token = token;
+    }
+
+    private String token;
+
+    @Override
+    public String getBotToken() {
+        return this.token;
+    }
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer() {
+        return this;
+    }
+
+    @Override
+    public void consume(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage sendMessage = SendMessage.builder()
+                    .chatId(chatId)
+                    .text(message)
+                    .build();
+
+            try {
+                telegramClient.execute(sendMessage);
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void sendMessage(SendMessage sendMessage) {
+        try {
+            telegramClient.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
